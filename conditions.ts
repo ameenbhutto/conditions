a55220c626ac4ce5624985ce41907031b59ccb3c
M Ameen Bhutto <159920673+ameenbhutto@users.noreply.github.com>
2024-04-02 15:45:40 -0700

    if and else

---
+++ conditions.ts
@@ -0,0 +1,8 @@
+// simple if else example
+var time = '9:00 to 12:00';
+if (time === '9:00 to 12:00') {
+    console.log('attend your class');
+}
+ else {
+     console.log('do not allend your class');
+    }
